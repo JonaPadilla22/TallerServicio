ccb306a74b041a43feb75ccf142769f0ba8dd341
jona22 <jonathan_pad22@hotmail.com>
2022-10-03 09:32:57 -0600

    estructura de app

---
+++ administrativo/app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxQRCodeModule } from "@techiediaries/ngx-qrcode";
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxQRCodeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
