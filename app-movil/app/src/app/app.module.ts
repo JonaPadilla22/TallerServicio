8a506c7f0dc6c894dd9267087205a0dd89ee945e
jona22 <jonathan_pad22@hotmail.com>
2022-10-03 09:22:53 -0600

    avance - navegacion y QR

--- app-movil/app/src/app/app.module.ts
+++ app-movil/app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+//import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @NgModule({
   declarations: [AppComponent],
