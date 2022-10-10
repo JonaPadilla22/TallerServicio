50587ce9ec4716fbafdf4a9ada2d3e4391cf0913
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-10-10 13:32:03 -0600

    A mitad del login
    Empezando el disenio de login antes de empezar a agregar los input

--- administrativo/app/src/app/app.module.ts
+++ administrativo/app/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxQRCodeModule } from "@techiediaries/ngx-qrcode";
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    NgxQRCodeModule
+    NgxQRCodeModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
