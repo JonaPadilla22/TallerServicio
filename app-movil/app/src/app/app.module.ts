994697f51e824e961b179dbe6dfcf6d30dab6da1
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-10-23 11:16:53 -0600

    Terminado prueba notif push

--- app-movil/app/src/app/app.module.ts
+++ app-movil/app/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FirebaseService } from './inicio/firebase.service';
 //import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    FirebaseService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
